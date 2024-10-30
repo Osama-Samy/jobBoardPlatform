ac7a887388c219eae3aa2b53efa4e262df6672ad
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2024-10-30 19:43:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ import applicationRouter from "./src/modules/application/application.routes.js"
 const app = express()
 app.use(express.json())
 
-app.get("*", (req, res) => {
-    res.send({message: "not found"})
-})
-
 app.use("/api/jobs", jobRouter)
 app.use("/api/candidates", candidateRouter)
 app.use("/api/applications", applicationRouter)
 
+app.get("*", (req, res) => {
+    res.send({message: "not found"})
+})
 
 app.listen(3000, () => {
     console.log("Server is running successfully")
